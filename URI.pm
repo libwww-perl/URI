d1f9c920e0db25e6ba4164383a36444c790c9b81
Gisle Aas <aas@oslonett.no>
1998-09-04 20:06:08 +0000

    Stringy version.

--- URI.pm
+++ URI.pm
@@ -1,4 +1,4 @@
-package URI;  # $Id: URI.pm,v 1.7 1998/09/03 20:58:38 aas Exp $
+package URI;  # $Id: URI.pm,v 1.7.2.1 1998/09/04 20:06:08 aas Exp $
 
 use strict;
 use vars qw($VERSION $DEFAULT_SCHEME $STRICT $DEBUG);
@@ -17,18 +17,13 @@ my $reserved   = q(;/?:@&=+$,);
 my $mark       = q(-_.!~*'());                                    #'; emacs
 my $unreserved = "A-Za-z0-9\Q$mark\E";
 
-use vars qw($uric $pchar $achar $ppchar);
+use vars qw($uric $pchar $achar $ppchar $scheme_re);
 $uric   = "\Q$reserved\E$unreserved%";
-$pchar  = $uric;  $pchar =~ s,\\[/?;],,g;
-$achar  = $uric;  $achar =~ s,\\[/?],,g;
-$ppchar = $uric;  $ppchar =~ s,\\?,,g;
+$pchar  = $uric;  $pchar  =~ s,\\[/?;],,g;
+$achar  = $uric;  $achar  =~ s,\\[/?],,g;
+$ppchar = $uric;  $ppchar =~ s,\\[?],,g;
 
-my $scheme_re = '[a-zA-Z][a-zA-Z0-9.+\-]*';
-
-use fields qw(xstr);
-use fields qw(scheme specific fragment);
-use fields qw(base);
-#use fields qw(_orig_uri);
+$scheme_re = '[a-zA-Z][a-zA-Z0-9.+\-]*';
 
 #print "$uric\n$achar\n$pchar\n";
 
@@ -54,8 +49,9 @@ sub new
 
 	# We need a scheme to determine which class to use
         my $scheme;
-        $scheme = $1 if $url =~ m/^($scheme_re):/o;
-	unless ($scheme) {
+	if ($url =~ m/^($scheme_re):/so) {
+	    $scheme = $1;
+	} else {
             if (ref $base){
                 $scheme = $base->scheme;
 	    } elsif ($base && $base =~ m/^($scheme_re):/o) {
@@ -70,10 +66,9 @@ sub new
         unless ($impclass) {
             Carp::croak("URI scheme '$scheme' is not supported")
 		if $STRICT;
-            # use generic as fallback
-            require URI::_generic;
-            $impclass = 'URI::_generic';
-            implementor($scheme, $impclass);  # register it
+	    
+	    require URI::_generic;
+	    $impclass = 'URI::_generic';
         }
         # hand-off to scheme specific implementation sub-class
         $self = $impclass->_init($url, $base, $scheme);
@@ -86,33 +81,15 @@ sub _init
 {
     my $class = shift;
     my($str, $base, $scheme) = @_;
-    my URI $self = bless [], $class;
-    #$self->{'_scheme'} = $scheme;
-    #$self->{'_orig_uri'} = $str if $DEBUG;
-    $self->base($base) if $base;
-    $self->_parse($str);
+    $str =~ s/([^$uric\#])/$URI::Escape::escapes{$1}/go;
+    my $self = bless \$str, $class;
     $self;
-}
-
-
-sub _parse
-{
-    my URI $self = shift;
-    my $str = shift;
-    # <scheme>:<scheme-specific-part>
-    $self->{'scheme'} = $1 if $str =~ s/^($scheme_re)://o;
-    $self->{'fragment'}   = $1 if $str =~ s/\#(.*)//s;
-    $self->{'specific'} = $str;
 }
 
 
 sub implementor
 {
     my($scheme, $impclass) = @_;
-    unless (defined $scheme) {
-        require URI::_generic;
-        return 'URI::_generic';
-    }
     $scheme = lc($scheme);
 
     if ($impclass) {
@@ -134,7 +111,7 @@ sub implementor
     unless (defined @{"${ic}::ISA"}) {
         # Try to load it
         eval "require $ic";
-        die $@ if $@ && $@ !~ /Can\'t locate $ic/;
+        die $@ if $@ && $@ !~ /Can\'t locate.*in \@INC/;
         return unless defined @{"${ic}::ISA"};
     }
 
@@ -155,127 +132,118 @@ sub _init_implementor
 sub clone
 {
     my $self = shift;
-    # this work as long as none of the components are references themselves
-    bless [ @$self ], ref $self;
+    my $other = $$self;
+    bless \$other, ref $self;
 }
 
 
 sub scheme
 {
-    my URI $self = shift;
-    my $old = $self->{'scheme'};
-    if (@_) {
-	my $new_scheme = shift;
-	if (defined($new_scheme) && length($new_scheme)) {
-	    # reparse URI with new scheme
-	    my $str = $self->as_string;
-	    $str =~ s/^$scheme_re://o;
-	    my $newself = URI->new("$new_scheme:$str");
-	    @$self = @$newself;  # copy content
-	    bless $self, ref($newself);
-	} else {
-	    $self->{'scheme'} = undef;
-	}
+    my $self = shift;
+
+    unless (@_) {
+	return unless $$self =~ /^($scheme_re):/o;
+	return $1;
+    }
+
+    my $old;
+    $old = $1 if $$self =~ s/^($scheme_re)://o;
+
+    my $new = shift;
+    if (defined($new) && length($new)) {
+	die "Bad scheme '$new'" unless $new =~ /^$scheme_re$/o;
+	my $newself = URI->new("$new:$$self");
+	$$self = $$newself; 
+	bless $self, ref($newself);
+    }
+
+    return $old;
+}
+
+
+sub opaque_part
+{
+    my $self = shift;
+
+    unless (@_) {
+	$$self =~ /^(?:$scheme_re:)?([^\#]*)/o or die;
+	return $1;
+    }
+
+    $$self =~ /^($scheme_re:)?    # optional scheme
+	        ([^\#]*)          # opaque
+                (\#.*)?           # optional fragment
+              $/sx or die;
+
+    my $old_scheme = $1;
+    my $old_opaque = $2;
+    my $old_frag   = $3;
+
+    my $new_opaque = shift;
+    $new_opaque = "" unless defined $new_opaque;
+    $new_opaque =~ s/([^$uric])/$URI::Escape::escapes{$1}/go;
+
+    $$self = "";
+    $$self .= $old_scheme if defined $old_scheme;
+    $$self .= $new_opaque;
+    $$self .= $old_frag if defined $old_frag;
+
+    $old_opaque;
+}
+
+
+sub fragment
+{
+    my $self = shift;
+    unless (@_) {
+	return unless $$self =~ /\#(.*)/s;
+	return $1;
+    }
+
+    my $old;
+    $old = $1 if $$self =~ s/\#(.*)//s;
+
+    my $new_frag = shift;
+    if (defined $new_frag) {
+	$new_frag =~ s/([^$uric])/$URI::Escape::escapes{$1}/go;
+	$$self .= "#$new_frag";
     }
     $old;
 }
 
-sub _accessor
-{
-    my $class = shift;
-    my $fno   = shift;
-    sub {
-	my URI $self = shift;
-	print "$self --> @_\n";
-	my $old = $self->[$fno];
-	if (@_) {
-	    $self->[$fno] = shift;
-	    $self->{xstr} = undef;
-	}
-	$old;
-    };
-}
-
-sub make_accessor_methods
-{
-    my $class = shift;
-    no strict 'refs';
-    my $fields = \%{"$class\::FIELDS"};
-    my $field;
-    for $field (@_) {
-	my $fno = $fields->{$field} ||
-	    die "No field called '$field' for class $class";
-	#print "$class\::$field --> $fno\n";
-	*{"$class\::$field"} = $class->_accessor($fno);
-    }
-}
-
-URI->make_accessor_methods(qw(fragment));
-
 
 sub as_string
 {
-    my URI $self = shift;
-    if (my $str = $self->{xstr}) {
-	return $str;  # cached
-    }
-    $self->{'xstr'} = $self->_as_string;  # set cache and return
+    my $self = shift;
+    $$self;
 }
 
 
-sub _as_string
+sub canonical
 {
-    my URI $self = shift;
-    my $str = "";
+    my $self = shift;
+    my $other = $self->clone;
 
-    # XXX Argh... this should not have to be rewritten...
-    #my($scheme, $specific, $fragment) = @{$self}{qw(scheme specific fragment)};
-    my $scheme   = $self->{scheme};
-    my $specific = $self->{specific};
-    my $fragment = $self->{fragment};
-
-    $str = "$scheme:" if $scheme;
-    $specific =~ s/([^$uric])/$URI::Escape::escapes{$1}/go;
-    $str .= $specific;
-    if (defined $fragment) {
-	$fragment =~ s/([^$uric])/$URI::Escape::escapes{$1}/go;
-	$str .= "#$fragment";
+    # Make sure scheme is lowercased
+    my $scheme = $other->scheme;
+    if ($scheme =~ /[A-Z]/) {
+	$other->scheme(lc $scheme);
     }
-    $str;
+
+    $other;
 }
-
-
-sub base {
-    my URI $self = shift;
-    my $base  = $self->{base};
-
-    if (@_) { # set
-	my $new_base = shift;
-	$new_base = $new_base->abs if ref($new_base);  # ensure absoluteness
-	$self->{base} = $new_base;
-    }
-    return unless defined wantarray;
-
-    # The base attribute supports 'lazy' conversion from URI strings
-    # to URI objects. Strings may be stored but when a string is
-    # fetched it will automatically be converted to a URI object.
-    # The main benefit is to make it much cheaper to say:
-    #   URI->new($random_url_string, 'http:')
-    if (defined($base) && !ref($base)) {
-	$self->{base} = $base = URI->new($base);
-    }
-    $base;
-}
-
 
 # Compare two URIs, subclasses will provide a more correct implementation
 sub eq {
     my($self, $other) = @_;
     $other = URI->new($other, $self) unless ref $other;
-    # XXX schemes should be compared case-insensitively
     ref($self) eq ref($other) &&                # same class
-	$self->as_string eq $other->as_string;  # same string
+	$self->canonical->as_string eq $other->canonical->as_string;
 }
+
+1;
+
+__END__
 
 # This is set up as an alias for various methods
 sub _bad_access_method
@@ -292,14 +260,14 @@ sub _bad_access_method
 }
 
 # generic-URI accessor methods
-*authority = \&_bad_access_method;
-*userinfo  = \&_bad_access_method;
-*host      = \&_bad_access_method;
-*port      = \&_bad_access_method;
-*abs_path  = \&_bad_access_method;
-*path      = \&_bad_access_method;
-*path_segments = \&_bad_access_method;
-*query     = \&_bad_access_method;
+*authority      = \&_bad_access_method;
+*userinfo       = \&_bad_access_method;
+*host           = \&_bad_access_method;
+*port           = \&_bad_access_method;
+*abs_path_query = \&_bad_access_method;
+*path           = \&_bad_access_method;
+*path_segments  = \&_bad_access_method;
+*query          = \&_bad_access_method;
 
 # generic-URI transformation methods
 sub abs { shift->clone; }
