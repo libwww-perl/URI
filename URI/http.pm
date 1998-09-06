5faffb97073a0e5037930ba9c1deb1b80814599b
Gisle Aas <aas@oslonett.no>
1998-09-06 19:05:19 +0000

    query_form() method moved to _query.pm.

--- URI/http.pm
+++ URI/http.pm
@@ -24,28 +24,4 @@ sub keywords
     map { uri_unescape($_) } split(/\+/, $old, -1);
 }
 
-# Handle ...?foo=bar&bar=foo type of query
-sub query_form {
-    my $self = shift;
-    my $old = $self->query;
-    if (@_) {
-        # Try to set query string
-        my @query;
-        while (my($key,$vals) = splice(@_, 0, 2)) {
-            $key = '' unless defined $key;
-            $key =~ s/(\W)/$escapes{$1}/g;
-            $vals = [$vals] unless ref $vals;
-            for my $val (@$vals) {
-                $val = '' unless defined $val;
-                $val =~ s/(\W)/$escapes{$1}/g;
-                push(@query, "$key=$val");
-            }
-        }
-        $self->query(join('&', @query));
-    }
-    return if !defined($old) || !length($old) || !defined(wantarray);
-    map { s/\+/ /g; uri_unescape($_) }
-         map { /=/ ? split(/=/, $_, 2) : ($_ => '')} split(/&/, $old);
-}
-
 1;
