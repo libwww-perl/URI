9eac63d97dd90fefe7b6d2f889b57b882a9089ba
Gisle Aas <aas@oslonett.no>
1998-09-06 22:24:11 +0000

    Most of it is new.

--- URI/mailto.pm
+++ URI/mailto.pm
@@ -9,21 +9,41 @@ use strict;
 sub to
 {
     my $self = shift;
-    my $old = $self->opaque_part;
+    my @old = $self->headers;
     if (@_) {
-	my $new = shift;
-	$new = "" unless defined $new;
-	$old =~ s/^[^?]+//;
-	$self->opaque_part("$new$old");
+	my @new = @old;
+	# get rid of any other to: fields
+	for (my $i = 0; $i < @new; $i += 2) {
+	    if (lc($new[$i]) eq "to") {
+		splice(@new, $i, 2);
+		redo;
+	    }
+	}
+
+	my $to = shift;
+	$to = "" unless defined $to;
+	unshift(@new, "to" => $to);
+	$self->headers(@new);
     }
-    $old =~ s/\?.*//s;
-    $old;
+    return unless defined wantarray;
+
+    my @to;
+    while (@old) {
+	my $h = shift @old;
+	my $v = shift @old;
+	push(@to, $v) if lc($h) eq "to";
+    }
+    join(",", @to);
 }
+
 
 sub headers
 {
     my $self = shift;
-    my @old = $self->query_form;
+
+    # The trick is to just treat everything as the query string...
+    my $opaque = "to=" . $self->opaque_part;
+    $opaque =~ s/\?/&/;
 
     if (@_) {
 	my @new = @_;
@@ -31,26 +51,21 @@ sub headers
 	# strip out any "to" fields
 	my @to;
 	for (my $i=0; $i < @new; $i += 2) {
-	    #print ">$i $old[$i]\n";
 	    if (lc($new[$i]) eq "to") {
 		push(@to, (splice(@new, $i, 2))[1]);  # remove header
 		redo;
 	    }
 	}
 
-	$self->query_form(@new);
-	$self->to(join(",",@to)) if @to;
+	my $new = join(",",@to);
+	$new =~ s/\?/%3F/g;  # protect any ?
+	$self->opaque_part($new);
+	$self->query_form(@new) if @new;
     }
+    return unless defined wantarray;
 
-    # strip out any "to" fields
-    for (my $i=0; $i < @old; $i += 2) {
-	#print ">$i $old[$i]\n";
-	if (lc($old[$i]) eq "to") {
-	    splice(@old, $i, 2);  # remove header
-	    redo;
-	}
-    }
-    @old;
+    # I am lazy today...
+    URI->new("mailto:?$opaque")->query_form;
 }
 
 1;
