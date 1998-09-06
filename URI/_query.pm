31b08e5d51fdd9b32bcc4386e833a9d8e64c189a
Gisle Aas <aas@oslonett.no>
1998-09-06 19:04:26 +0000

    Moved some functions out of _generic.pm and http.pm

---
+++ URI/_query.pm
@@ -0,0 +1,48 @@
+package URI::_query;
+
+use strict;
+use URI ();
+use URI::Escape qw(uri_unescape);
+
+sub query
+{
+    my $self = shift;
+    $$self =~ m,^([^?\#]*)(?:\?([^\#]*))?(.*)$,s or die;
+    
+    if (@_) {
+	my $q = shift;
+	$$self = $1;
+	if (defined $q) {
+	    $q =~ s/([^$URI::uric])/$URI::Escape::escapes{$1}/go;
+	    $$self .= "?$q";
+	}
+	$$self .= $3;
+    }
+    $2;
+}
+
+# Handle ...?foo=bar&bar=foo type of query
+sub query_form {
+    my $self = shift;
+    my $old = $self->query;
+    if (@_) {
+        # Try to set query string
+        my @query;
+        while (my($key,$vals) = splice(@_, 0, 2)) {
+            $key = '' unless defined $key;
+            $key =~ s/(\W)/$URI::Escape::escapes{$1}/g;
+            $vals = [$vals] unless ref $vals;
+            for my $val (@$vals) {
+                $val = '' unless defined $val;
+                $val =~ s/(\W)/$URI::Escape::escapes{$1}/g;
+                push(@query, "$key=$val");
+            }
+        }
+        $self->query(join('&', @query));
+    }
+    return if !defined($old) || !length($old) || !defined(wantarray);
+    map { s/\+/ /g; uri_unescape($_) }
+         map { /=/ ? split(/=/, $_, 2) : ($_ => '')} split(/&/, $old);
+}
+
+1;
