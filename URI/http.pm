7bd4b6e3e330f79be8855672d361d95bc2fc5b24
Gisle Aas <aas@oslonett.no>
1998-09-07 23:16:04 +0000

    canonical

--- URI/http.pm
+++ URI/http.pm
@@ -5,4 +5,18 @@ require URI::_server;
 
 sub default_port { 80 }
 
+sub canonical
+{
+    my $self = shift;
+    my $other = $self->SUPER::canonical;
+    if (defined($other->authority) &&
+        !length($other->path) && !defined($other->query)) {
+	$other = $other->clone if $other == $self;
+	$other->path("/");
+	return $other;
+    }
+    # XXX should also unescape any unreserved uric characters
+    $other;
+}
+
 1;
