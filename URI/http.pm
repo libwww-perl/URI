c2885c7bb09873dbca2bb19a6f229ee27ecaf7ab
Gisle Aas <aas@oslonett.no>
1998-09-08 13:34:37 +0000

    unescape any unreserved escape in canonical.

--- URI/http.pm
+++ URI/http.pm
@@ -3,19 +3,31 @@ package URI::http;
 require URI::_server;
 @ISA=qw(URI::_server);
 
+use strict;
+use vars qw(%unreserved_escape);
+
 sub default_port { 80 }
 
 sub canonical
 {
     my $self = shift;
     my $other = $self->SUPER::canonical;
-    if (defined($other->authority) &&
-        !length($other->path) && !defined($other->query)) {
+
+    my $slash_path = defined($other->authority) &&
+        !length($other->path) && !defined($other->query);
+
+    if ($slash_path || $$other =~ /%/) {
 	$other = $other->clone if $other == $self;
-	$other->path("/");
-	return $other;
+	unless (%unreserved_escape) {
+	    for ("A" .. "Z", "a" .. "z", "0" .."9",
+		 "-", "_", ".", "!", "~", "*", "'", "(", ")"
+		) {
+		$unreserved_escape{sprintf "%%%02X", ord($_)} = $_;
+	    }
+	}
+	$$other =~ s/(%[0-9A-F]{2})/$unreserved_escape{$1} || $1/ge;
+	$other->path("/") if $slash_path;
     }
-    # XXX should also unescape any unreserved uric characters
     $other;
 }
 
