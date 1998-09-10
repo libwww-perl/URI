d7d2578a8344c878331759794c90a3bf4d943dd3
Gisle Aas <aas@oslonett.no>
1998-09-10 13:10:06 +0000

    Sub-class for specific OS-types.

--- URI/file.pm
+++ URI/file.pm
@@ -1,12 +1,57 @@
 package URI::file;
 
-require URI::_server;
-@ISA=qw(URI::_server);  # or _generic?
-
 use strict;
+use vars qw(@ISA);
 
-sub path { shift->path_query(@_) }  # XXX
+require URI::_generic;
+@ISA = qw(URI::_generic);
+set_type($^O);
+
+sub set_type
+{
+    my($OS) = @_;
+    pop(@ISA) if @ISA > 1;
+
+    my $class = {
+		 os2     => "OS2",
+		 MacOS   => "Mac",
+		 MSWin32 => "Win32",
+		 Win32   => "Win32"
+		}->{$OS} || "Unix";
+    $class = "URI::file::$class";
+
+    eval "require $class";
+    warn $@ if $@;
+    die $@ if $@;
+    push(@ISA, $class);
+}
+
+sub path { shift->path_query(@_) }
+sub host { shift->authority(@_)  }
+
+sub new
+{
+    my($class, $path) = @_;
+    my $uri = URI->new("", "file");
+    if (my $host = $class->extract_host($path)) {
+	$uri->authority($host);
+    }
+    $uri->path_segments($class->split_path($path));
+    $$uri = "file:$$uri" if $$uri =~ m,^/,;  # fixup
+    $uri;
+}
+
+sub new_abs
+{
+    my $class = shift;
+    $class->new(@_)->abs($class->curdir);
+}
+
+sub curdir
+{
+    my $class = shift;
+    require Cwd;
+    $class->new(Cwd::fastcwd());
+}
 
 1;
-
-
