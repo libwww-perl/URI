b6510e570108eeacb7d7284a58fc3ec1ce1075ef
Gisle Aas <aas@oslonett.no>
1998-09-22 14:45:00 +0000

    New interface.

--- URI/file/Mac.pm
+++ URI/file/Mac.pm
@@ -1,15 +1,25 @@
 package URI::file::Mac;
 
+require URI::file::Base;
+@ISA=qw(URI::file::Base);
+
 use strict;
+use URI::Escape qw(uri_unescape);
 
 sub extract_authority
 {
-    undef;
+    my $class = shift;
+    # move volume part to authority
+    return $1 if $_[0] =~ s/^([^:]+:):*//;
+    return;
 }
 
-sub split_path
+
+sub extract_path
 {
-    my($class, $path) = @_;
+    my $class = shift;
+    my $path = shift;
+
     my @pre;
     if ($path =~ s/^(:+)//) {
 	if (length($1) == 1) {
@@ -17,47 +27,81 @@ sub split_path
 	} else {
 	    @pre = ("..") x (length($1) - 1);
 	}
-	return(@pre, "") unless length($path);
-	# XXX if $path now contains a sequence of "." and ".." we are
-	# now in trouble...
-    } else {
-	@pre = ("");
     }
-    (@pre, map { ($_ eq ".")  ? "%2E"    :
-                 ($_ eq "..") ? "%2E%2E" :
-		                do { s/%/%25/g; s/;/%3B/g; $_ }
-               } split(/:/, $path, -1));
+
+    $path =~ s,([%/;]),$URI::Escape::escapes{$1},g;
+
+    my @path = split(/:/, $path, -1);
+    for (@path) {
+	if ($_ eq "." || $_ eq "..") {
+	    $_ = "%2E" x length($_);
+	}
+    }
+    (join("/", @pre, @path), 1);
 }
+
 
 sub file
 {
-    shift;  # class
-    shift;  # authority;
-    for (@_) {
+    my $class = shift;
+    my $uri = shift;
+    my @path;
+
+    my $auth = $uri->authority;
+    if (defined $auth) {
+	if (lc($auth) ne "localhost") {
+	    my $u_auth = uri_unescape($auth);
+	    if ($u_auth =~ s/:$//) {
+		# volume
+		$u_auth =~ s/%/%25/g; # blææh (we will uri_unescape below)
+		@path = ("", $u_auth);
+	    } elsif (!$class->is_this_host($u_auth)) {
+		# some other host (use it as volume name)
+		@path = ("", $auth);
+		# XXX or just return to make it illegal;
+	    }
+	}
+    }
+    my @ps = split("/", $uri->path, -1);
+    shift @ps if @path;
+    push(@path, @ps);
+
+    my $pre = "";
+    if (!@path) {
+	return;  # empty path; XXX return ":" instead?
+    } elsif ($path[0] eq "") {
+	# absolute
+	shift(@path);
+	if (@path == 1) {
+	    return if $path[0] eq "";  # not root directory
+	    push(@path, "");           # volume only, effectively append ":"
+	}
+    } else {
+	$pre = ":";
+	while (@path) {
+	    next if $path[0] eq ".";
+	    last if $path[0] ne "..";
+	    $pre .= ":";
+	} continue {
+	    shift(@path);
+	}
+    }
+    return unless $pre || @path;
+    for (@path) {
+	s/;.*//;  # get rid of parameters
+	#return unless length; # XXX
+	$_ = uri_unescape($_);
 	return if /\0/;
 	return if /:/;  # Should we?
     }
-    my $pre = "";
-    if ($_[0] eq "") {
-	# absolute
-	shift;
-    } else {
-	$pre = ":";
-	while (@_) {
-	    next if $_[0] eq ".";
-	    last if $_[0] ne "..";
-	    $pre .= ":";
-	} continue {
-	    shift(@_);
-	}
-    }
-    $pre . join(":", @_);
+    $pre . join(":", @path);
 }
 
 sub dir
 {
     my $class = shift;
     my $path = $class->file(@_);
+    return unless defined $path;
     $path .= ":" unless $path =~ /:$/;
     $path;
 }
