1b5d09c90b5421cfcc583f7b6108eb542eb63369
Gisle Aas <gisle@aas.no>
2003-07-24 15:19:59 +0000

    Document URI::Split.

--- URI/Split.pm
+++ URI/Split.pm
@@ -39,3 +39,58 @@ sub uri_join {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+URI::Split - Parse and compose URI strings
+
+=head1 SYNOPSIS
+
+ use URI::Split qw(uri_split uri_join);
+ ($scheme, $auth, $path, $query, $frag) = uri_split($uri);
+ $uri = uri_join($scheme, $auth, $path, $query, $frag);
+
+=head1 DESCRIPTION
+
+The C<URI::Split> module provide functions to parse and compose URI
+strings.  The following functions are provided:
+
+=over
+
+=item ($scheme, $auth, $path, $query, $frag) = uri_split($uri)
+
+The uri_split() function will break up a URI string into it component
+parts.  An C<undef> value is returned for those parts that are not
+present.  The $path part will always be present (but can be the empty
+string) and will thus never be returned as C<undef>.
+
+No sensible value is returned if this function is called in scalar
+context.
+
+=item $uri = uri_join($scheme, $auth, $path, $query, $frag)
+
+The uri_join() function will put together a URI string from its parts.
+Missing parts are signaled by passing C<undef> for the corresponding
+argument.
+
+Minimal escaping will be applied to parts that contain reserved chars
+that would confuse a parser.  For instance any occurence of '?' or '#'
+in $path will always be escaped as they would otherwise be parsed back
+as query or fragment.
+
+=back
+
+=head1 SEE ALSO
+
+L<URI>, L<URI::Escape>
+
+=head1 COPYRIGHT
+
+Copyright 2003, Gisle Aas
+
+This library is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=cut
