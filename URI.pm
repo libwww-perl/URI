97619fce521bc42d9a1bc49bb61c6673bb74cf10
Gisle Aas <gisle@aas.no>
2004-11-05 14:17:33 +0000

    Release 1.35.

--- URI.pm
+++ URI.pm
@@ -2,7 +2,7 @@ package URI;
 
 use strict;
 use vars qw($VERSION);
-$VERSION = "1.34"; # $Date: 2004/11/05 14:09:21 $
+$VERSION = "1.35"; # $Date: 2004/11/05 14:17:33 $
 
 use vars qw($ABS_REMOTE_LEADING_DOTS $ABS_ALLOW_RELATIVE_SCHEME);
 
