6276f5c147cd70fef6820b48c52e08f2091a312a
Gisle Aas <gisle@aas.no>
2011-01-23 00:32:27 +0100

    Release 1.58

--- URI.pm
+++ URI.pm
@@ -2,7 +2,7 @@ package URI;
 
 use strict;
 use vars qw($VERSION);
-$VERSION = "1.57";
+$VERSION = "1.58";
 
 use vars qw($ABS_REMOTE_LEADING_DOTS $ABS_ALLOW_RELATIVE_SCHEME $DEFAULT_QUERY_FORM_DELIMITER);
 
