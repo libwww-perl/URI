7594213d5223b836f8dfa5978f19884335b91616
John Miller <john@rimmkaufman.com>
2011-01-22 18:33:39 +0100

    Distingush between empty and undef values in query_form [RT#62708]
    The _query module does not handle http query strings that are not
    <name>=<value> pairs.
    
    There is a behavior change. Previously, parameters sent into query_form
    with value undef would be set to blank. Now they will be included with
    no "=" or value. To set a value to blank, use an empty string -- ''.

--- URI/_query.pm
+++ URI/_query.pm
@@ -46,10 +46,14 @@ sub query_form {
 	    $key =~ s/ /+/g;
 	    $vals = [ref($vals) eq "ARRAY" ? @$vals : $vals];
             for my $val (@$vals) {
-                $val = '' unless defined $val;
-		$val =~ s/([;\/?:@&=+,\$\[\]%])/ URI::Escape::escape_char($1)/eg;
-                $val =~ s/ /+/g;
-                push(@query, "$key=$val");
+                if (defined $val) {
+		    $val =~ s/([;\/?:@&=+,\$\[\]%])/ URI::Escape::escape_char($1)/eg;
+		    $val =~ s/ /+/g;
+		    push(@query, "$key=$val");
+		}
+                else {
+                    push(@query, $key);
+                }
             }
         }
         if (@query) {
@@ -64,9 +68,8 @@ sub query_form {
         }
     }
     return if !defined($old) || !length($old) || !defined(wantarray);
-    return unless $old =~ /=/; # not a form
     map { s/\+/ /g; uri_unescape($_) }
-         map { /=/ ? split(/=/, $_, 2) : ($_ => '')} split(/[&;]/, $old);
+         map { /=/ ? split(/=/, $_, 2) : ($_ => undef)} split(/[&;]/, $old);
 }
 
 # Handle ...?dog+bones type of query
