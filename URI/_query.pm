a79d2760c1d613ae736267046de722fb11326838
Gisle Aas <gisle@aas.no>
2012-03-25 22:44:30 +0200

    Revert "Make query_form() encoded "\n" as "%0D%0A" for HTML/4.01 compliance"
    Not so sure any more that this was a good idea, so I'm reverting it
    for now [RT#75592]
    
    This reverts commit dd80e795b9b79f4884592c59f42329e8a7d34a7d.

--- URI/_query.pm
+++ URI/_query.pm
@@ -48,7 +48,6 @@ sub query_form {
 	    $vals = [ref($vals) eq "ARRAY" ? @$vals : $vals];
             for my $val (@$vals) {
                 $val = '' unless defined $val;
-		$val =~ s/(?<!\r)\n/\r\n/g;
 		$val =~ s/([;\/?:@&=+,\$\[\]%])/ URI::Escape::escape_char($1)/eg;
                 $val =~ s/ /+/g;
                 push(@query, "$key=$val");
@@ -67,7 +66,7 @@ sub query_form {
     }
     return if !defined($old) || !length($old) || !defined(wantarray);
     return unless $old =~ /=/; # not a form
-    map { s/\+/ /g; $_ = uri_unescape($_); s/\r\n/\n/g; $_ }
+    map { s/\+/ /g; uri_unescape($_) }
          map { /=/ ? split(/=/, $_, 2) : ($_ => '')} split(/[&;]/, $old);
 }
 
