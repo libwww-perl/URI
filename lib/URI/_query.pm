ee1146a03e541d9fb67d22959c8a06632fda1200
Olaf Alders <olaf@wundersolutions.com>
2023-08-23 10:13:49 -0400

    increment $VERSION after 5.20 release

--- lib/URI/_query.pm
+++ lib/URI/_query.pm
@@ -6,7 +6,7 @@ use warnings;
 use URI ();
 use URI::Escape qw(uri_unescape);
 
-our $VERSION = '5.20';
+our $VERSION = '5.21';
 
 sub query
 {
--- lib/URI/_userpass.pm
+++ lib/URI/_userpass.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use URI::Escape qw(uri_unescape);
 
-our $VERSION = '5.20';
+our $VERSION = '5.21';
 
 sub user
 {
