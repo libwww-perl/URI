52d5ddfed12770e31e31a4de2e8f95f5eeb01098
David Dick <ddick@cpan.org>
2025-07-19 15:07:27 +1000

    Adding Websocket (ws) URI

---
+++ lib/URI/ws.pm
@@ -0,0 +1,10 @@
+package URI::ws;
+
+use strict;
+use warnings;
+
+our $VERSION = '5.33';
+
+use parent 'URI::http';
+
+1;
---
+++ lib/URI/wss.pm
@@ -0,0 +1,10 @@
+package URI::wss;
+
+use strict;
+use warnings;
+
+our $VERSION = '5.33';
+
+use parent 'URI::https';
+
+1;
